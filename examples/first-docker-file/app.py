8b41c818496a30f4fe6ddf01b037dc4fa9647d99
Vijji455 <149236290+Vijji455@users.noreply.github.com>
2026-04-23 22:03:50 +0530

    Update app.py

--- examples/first-docker-file/app.py
+++ examples/first-docker-file/app.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Docker 🚀"
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+  
